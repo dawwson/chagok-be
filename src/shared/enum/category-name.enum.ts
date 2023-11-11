6b41e2e04ea307d97f1633ecc2381e9735fdc85b
dawwson <shines1427@gmail.com>
2023-11-11 22:36:09 +0900

    Add: category name에 대한 enum 작성

---
+++ src/shared/enum/category-name.enum.ts
@@ -0,0 +1,11 @@
+export enum CategoryName {
+  FOOD = '식비',
+  TRANSPORTATION = '교통',
+  COMMUNICATION = '통신',
+  HEALTH_CARE = '의료',
+  HOUSING = '주거',
+  ENTERTAINMENT = '문화',
+  APPAREL = '의류',
+  EDUCATION = '교육',
+  ETC = '기타',
+}

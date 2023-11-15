6ef501b0afde4647f2f511999bbb3688eee59be7
dawwson <shines1427@gmail.com>
2023-11-15 23:45:54 +0900

    Fix: 지출 엔티티 모델링 변경

--- src/entity/expense.entity.ts
+++ src/entity/expense.entity.ts
@@ -2,7 +2,6 @@ import {
   Check,
   Column,
   CreateDateColumn,
-  DeleteDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
@@ -11,7 +10,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { User } from './user.entity';
-import { BudgetMonth } from '../shared/enum/budget-month.enum';
+import { Budget } from './budget.entity';
 import { Category } from './category.entity';
 
 @Entity('expenses')
@@ -24,9 +23,22 @@ export class Expense {
   @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => Budget)
+  @JoinColumn({ name: 'budget_id' })
+  budget: Budget;
+
+  @Column()
+  budgetId: number;
+
   @OneToOne(() => Category, { nullable: false })
   @JoinColumn({ name: 'category_id' })
   category: Category;
+
+  @Column()
+  categoryId: number;
 
   @Column({ type: 'varchar', length: 100 })
   content: string;
@@ -37,18 +49,12 @@ export class Expense {
   @Column({ type: 'boolean', default: false })
   isExcluded: boolean;
 
-  @Column({ type: 'varchar', length: 10 })
-  year: string;
-
-  @Column({ type: 'varchar', length: 10 })
-  month: BudgetMonth;
+  @Column({ type: 'timestamp with time zone' })
+  expenseDate: Date;
 
   @CreateDateColumn({ type: 'timestamp with time zone' })
   createdAt: Date;
 
   @UpdateDateColumn({ type: 'timestamp with time zone', nullable: true })
   updatedAt: Date;
-
-  @DeleteDateColumn({ type: 'timestamp with time zone', nullable: true })
-  deletedAt: Date;
 }

f99141e41aa1ffe65acebb076eb53066771bcaa5
dawwson <shines1427@gmail.com>
2023-11-16 18:28:48 +0900

    Refactor: categoryId가 유효하지 않을 경우의 예외처리 추가

--- src/api/expense/service/expense.service.ts
+++ src/api/expense/service/expense.service.ts
@@ -1,23 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { CreateExpenseResource } from '../dto/create-expense-resource.dto';
 
 import { Expense } from '../../../entity/expense.entity';
-import { Budget } from '../../../entity/budget.entity';
 import { BudgetMonth } from '../../../shared/enum/budget-month.enum';
+import { Category } from '../../../entity/category.entity';
+import { FailMessage } from '../../../shared/enum/fail-message.enum';
 
 @Injectable()
 export class ExpenseService {
   constructor(
     @InjectRepository(Expense)
     private readonly expenseRepo: Repository<Expense>,
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Expense>,
   ) {}
 
   async createExpenseData(
     createExpenseResource: CreateExpenseResource,
   ): Promise<Expense> {
+    const category = await this.categoryRepo.findOneBy({
+      id: createExpenseResource.categoryId,
+    });
+
+    if (!category) {
+      throw new BadRequestException(FailMessage.EXPENSE_INVALID_CATEGORY_ID);
+    }
+
     return this.expenseRepo.save(
       this.expenseRepo.create({ ...createExpenseResource }),
     );

2a2ff734e96057facdee88818821e372e511691e
dawwson <shines1427@gmail.com>
2024-09-27 16:57:37 +0900

    Fix: category, budget-category 엔티티 설계 변경

--- src/entity/category.entity.ts
+++ src/entity/category.entity.ts
@@ -1,5 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { CategoryName } from '../shared/enum/category-name.enum';
+import { CategoryName } from '@src/shared/enum/category-name.enum';
+import { TxType } from '@src/shared/enum/tx-type.enum';
 
 @Entity('categories')
 export class Category {
@@ -8,6 +9,9 @@ export class Category {
 
   @Column({ type: 'varchar', length: 100, unique: true })
   name: CategoryName;
+
+  @Column({ type: 'varchar', length: 20 })
+  type: TxType;
 
   @CreateDateColumn({ type: 'timestamp with time zone' })
   createdAt: Date;

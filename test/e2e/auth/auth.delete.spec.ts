a3df1605980792e2795150c74c93377d68682220
dawwson <shines1427@gmail.com>
2024-09-29 18:18:39 +0900

    Test: 인증 api e2e 테스트 작성

--- test/e2e/auth/auth.delete.spec.ts
+++ test/e2e/auth/auth.delete.spec.ts
@@ -1,10 +1,11 @@
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 
-import { clearDatabase, createTestApp, getRepository, setupDatabase } from '@test/utils/utils';
-import { testUsers } from '@test/in-memory-testing/test-data';
-
 import { User } from '@src/entity/user.entity';
+import { clearDatabase, createTestApp, getRepository, setupDatabase } from '@test/utils/utils';
+import { testUsers } from '@test/utils/test-data';
+
+const API_URL = '/auth';
 
 describe('DELETE /auth', () => {
   let app: INestApplication;
@@ -23,22 +24,22 @@ describe('DELETE /auth', () => {
   });
 
   describe('/account', () => {
-    test('로그인 전: (401) 유효하지 않은 JWT', async () => {
+    test('로그인 전: (401) 유효하지 않은 토큰', async () => {
       await request(app.getHttpServer()) //
-        .delete('/auth/account') //
+        .delete(`${API_URL}/account`) //
         .expect(401);
     });
 
     describe('사용자1 로그인 후:', () => {
       let agent: request.SuperAgentTest;
-      const user = testUsers[0];
+      const currentUser = testUsers[0];
 
       beforeAll(async () => {
         const res = await request(app.getHttpServer())
-          .post('/auth/sign-in')
+          .post(`${API_URL}/sign-in`)
           .send({
-            email: user.email,
-            password: user.password,
+            email: currentUser.email,
+            password: currentUser.password,
           })
           .expect(200);
 
@@ -50,13 +51,13 @@ describe('DELETE /auth', () => {
         // given
 
         // when
-        const res = await agent.delete('/auth/account');
+        const res = await agent.delete(`${API_URL}/account`);
 
         // then
         expect(res.status).toBe(204);
         expect(res.get('Set-Cookie')[0]).toBe('accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
 
-        const find = await getRepository(User).findOne({ where: { id: user.id } });
+        const find = await getRepository(User).findOne({ where: { id: currentUser.id } });
         expect(find).toBeNull();
       });
     });

c2533f928f6459ab6f62083907bdcb6096ad736e
dawwson <shines1427@gmail.com>
2024-09-29 19:44:09 +0900

    Fix: 카테고리 타입 분류

--- src/database/seeding/seeds/category.seeder.ts
+++ src/database/seeding/seeds/category.seeder.ts
@@ -2,21 +2,21 @@ import { DataSource } from 'typeorm';
 import { Seeder } from 'typeorm-extension';
 
 import { Category } from '@src/entity/category.entity';
-import { CategoryName } from '@src/shared/enum/category-name.enum';
+import { IncomeCategoryName, ExpenseCategoryName } from '@src/shared/enum/category-name.enum';
 
 export default class CategorySeeder implements Seeder {
   async run(dataSource: DataSource): Promise<any> {
     // 테이블 데이터 초기화(스키마 유지)
-    await dataSource.query(
-      'TRUNCATE TABLE categories, budget_category, expenses, budgets, users RESTART IDENTITY CASCADE',
+    await dataSource.query('TRUNCATE TABLE categories, budget_category, txs, budgets, users RESTART IDENTITY CASCADE');
+
+    const incomeCategories = Object.keys(IncomeCategoryName).map((key) =>
+      Category.createIncomeCategory(IncomeCategoryName[key]),
     );
 
-    const categories = Object.keys(CategoryName).map((key) => {
-      const category = new Category();
-      category.name = CategoryName[key];
-      return category;
-    });
+    const expenseCategories = Object.keys(ExpenseCategoryName).map((key) =>
+      Category.createExpenseCategory(ExpenseCategoryName[key]),
+    );
 
-    await dataSource.getRepository(Category).save(categories);
+    await dataSource.getRepository(Category).save([...incomeCategories, ...expenseCategories]);
   }
 }
--- src/entity/category.entity.ts
+++ src/entity/category.entity.ts
@@ -1,5 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { CategoryName } from '@src/shared/enum/category-name.enum';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IncomeCategoryName, ExpenseCategoryName } from '@src/shared/enum/category-name.enum';
 import { TxType } from '@src/shared/enum/tx-type.enum';
 
 @Entity('categories')
@@ -8,8 +8,22 @@ export class Category {
   id: number;
 
   @Column({ type: 'varchar', length: 100, unique: true })
-  name: CategoryName;
+  name: IncomeCategoryName | ExpenseCategoryName;
 
   @Column({ type: 'varchar', length: 20 })
   type: TxType;
+
+  static createIncomeCategory(name: IncomeCategoryName) {
+    const category = new Category();
+    category.name = name;
+    category.type = TxType.INCOME;
+    return category;
+  }
+
+  static createExpenseCategory(name: ExpenseCategoryName) {
+    const category = new Category();
+    category.name = name;
+    category.type = TxType.EXPENSE;
+    return category;
+  }
 }

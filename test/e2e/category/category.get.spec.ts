4c5a0b732b91dcc133271b2e0d1a5d35a2d13323
dawwson <shines1427@gmail.com>
2024-10-07 05:06:11 +0900

    Test: 카테고리 목록 조회 API e2e 테스트 코드 작성

--- test/e2e/category/category.get.spec.ts
+++ test/e2e/category/category.get.spec.ts
@@ -1,89 +1,71 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { DataSource } from 'typeorm';
-import { IBackup, IMemoryDb } from 'pg-mem';
 import * as request from 'supertest';
-import * as cookieParser from 'cookie-parser';
 
-import { setupMemoryDb } from '@test/in-memory-testing/setup-memory-db';
-import { initializeDataSource } from '@test/in-memory-testing/initialize-data-source';
-import { setupTestData } from '@test/in-memory-testing/setup-test-data';
-import { InMemoryTestingModule } from '@test/in-memory-testing/in-memory-testing.module';
-import { testCategories, testUsers } from '@test/in-memory-testing/test-data';
+import { User } from '@src/entity/user.entity';
+import { clearDatabase, createTestApp, setupDatabase } from '@test/utils/utils';
+import { testUsers } from '@test/utils/test-data';
+import { ExpenseCategoryName, IncomeCategoryName } from '@src/shared/enum/category-name.enum';
+import { TxType } from '@src/shared/enum/tx-type.enum';
 
-describe('/categories (GET)', () => {
+const API_URL = '/categories';
+
+describe(`GET ${API_URL}`, () => {
   let app: INestApplication;
-  let dataSource: DataSource;
-  let memoryDb: IMemoryDb;
-  let backup: IBackup;
 
   beforeAll(async () => {
-    // 1. DB 설정
-    memoryDb = setupMemoryDb();
-    // 2. 연결 설정된 dataSource를 가져옴
-    dataSource = await initializeDataSource(memoryDb);
-    // 3. 테스트에 필요한 데이터 생성
-    await setupTestData(dataSource);
-
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [InMemoryTestingModule],
-    })
-      .overrideProvider(DataSource)
-      .useValue(dataSource)
-      .compile();
-
-    app = moduleFixture.createNestApplication();
-    app.use(cookieParser());
-    await app.init();
-
-    // NOTE: datasource initialize 한 시점의 데이터를 백업합니다.
-    backup = memoryDb.backup();
+    await setupDatabase();
+    app = await createTestApp();
   });
 
   afterEach(async () => {
-    // NOTE: 매 테스트 종료 후 백업한 데이터로 ROLLBACK 합니다.
-    backup.restore();
+    clearDatabase();
   });
 
   afterAll(async () => {
-    // NOTE: 모든 테스트 종료 후 앱을 종료합니다.(+ connection closed)
     await app.close();
   });
 
-  describe('테스트 사용자 1로 로그인 후', () => {
+  describe('로그인 전 : (401) 유효하지 않은 토큰', () => {
+    test('/', async () => {
+      await request(app.getHttpServer()) //
+        .get(`${API_URL}`) //
+        .expect(401);
+    });
+  });
+
+  describe('로그인 후 : (사용자 1)', () => {
     let agent: request.SuperAgentTest;
+    const currentUser = testUsers[0];
 
     beforeAll(async () => {
       const res = await request(app.getHttpServer())
         .post('/auth/sign-in')
         .send({
-          email: testUsers[0].email,
-          password: testUsers[0].password,
+          email: currentUser.email,
+          password: currentUser.password,
         })
         .expect(200);
 
-      // Cookie 헤더에 JWT를 유지하여 사용할 agent 생성
       agent = request.agent(app.getHttpServer());
       agent.set('Cookie', res.get('Set-Cookie'));
     });
 
-    describe('GET /categories', () => {
-      test('카테고리 목록 조회 성공(200)', async () => {
-        // given
+    test('(200) 카테고리 목록 조회 성공', async () => {
+      // given
 
-        // when
-        const res = await agent //
-          .get('/categories') //
-          .expect(200);
+      // when
+      const res = await agent.get(`${API_URL}`);
 
-        // then
-        expect(res.body.data).toHaveLength(testCategories.length);
-        res.body.data.forEach((category) => {
-          expect(category).toEqual({
-            id: expect.any(Number),
-            name: expect.any(String),
-          });
+      // then
+      expect(res.status).toBe(200);
+      res.body.data.forEach((category) => {
+        expect(category).toEqual({
+          id: expect.any(Number),
+          name: expect.any(String),
+          type: expect.any(String),
         });
+        expect([...Object.values(IncomeCategoryName), ...Object.values(ExpenseCategoryName)]).toContain(category.name);
+        expect(Object.values(TxType)).toContain(category.type);
       });
     });
   });

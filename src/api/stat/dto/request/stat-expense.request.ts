5c27a06631e834c1fcdb27d9cbfe14e8c6acd6ab
dawwson <shines1427@gmail.com>
2024-12-22 23:38:12 +0900

    Feat: stat controller api docs 작성

--- src/api/stat/dto/request/stat-expense.request.ts
+++ src/api/stat/dto/request/stat-expense.request.ts
@@ -3,14 +3,18 @@ import { IsDefined, IsIn, Max, Min } from 'class-validator';
 import * as dayjs from 'dayjs';
 
 import { ErrorCode } from '@src/shared/enum/error-code.enum';
+import { ApiBudgetMonth, ApiBudgetYear } from '@src/shared/decorator/api-custom-property.decorator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class StatExpenseRequestParam {
+  @ApiBudgetYear({ description: '조회할 지출 통계 연도 (YYYY)' })
   @IsDefined({ message: ErrorCode.MISSING_PARAMETER })
   @Transform(({ value }) => parseInt(value)) // URL에서 문자열로 전달되므로 Number로 형변환
   @Min(dayjs().year(), { message: ErrorCode.BUDGET_YEAR_OUT_OF_RANGE })
   @Max(dayjs().year() + 1, { message: ErrorCode.BUDGET_YEAR_OUT_OF_RANGE })
   year: number;
 
+  @ApiBudgetMonth({ description: '조회할 지출 통계 월 (M~MM)' })
   @IsDefined({ message: ErrorCode.MISSING_PARAMETER })
   @Transform(({ value }) => parseInt(value)) // URL에서 문자열로 전달되므로 Number로 형변환
   @IsDefined({ message: ErrorCode.MISSING_PARAMETER })
@@ -20,6 +24,11 @@ export class StatExpenseRequestParam {
 }
 
 export class StatExpenseRequestQuery {
+  @ApiProperty({
+    description: '조회할 지출 통계의 유형',
+    enum: ['monthly', 'yearly'],
+    example: 'monthly',
+  })
   @IsDefined({ message: ErrorCode.MISSING_PARAMETER })
   @IsIn(['monthly', 'yearly'], { message: ErrorCode.STAT_INVALID_VIEW })
   view: 'monthly' | 'yearly';
